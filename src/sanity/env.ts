2bfd73115be8350acb57fff461d711055dda134f
Kurays <christnarvarte15@gmail.com>
2026-07-16 23:22:03 +0800

    fix: fallback to project defaults if env variables are not present during build

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,19 +1,7 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-01-01";
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET",
-);
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID",
-);
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "f5dvap5r";
 
-function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage);
-  }
-  return v;
-}
